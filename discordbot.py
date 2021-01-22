13517d55f743290f7512323136c7a668c78ce961
Admern <43789985+Admern@users.noreply.github.com>
2021-01-22 23:50:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,17 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-# 返信する非同期関数を定義
-async def reply(message):
-    reply = f'{message.author.mention} hi' # 返信メッセージの作成
-    await message.channel.send(reply) # 返信メッセージを送信
-
-# 発言時に実行されるイベントハンドラを定義
-@bot.event
-async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
