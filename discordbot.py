637786b43a5614be4d8fd4c2f02a3f3ee7c8b86b
Admern <43789985+Admern@users.noreply.github.com>
2021-01-23 11:42:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,12 +9,22 @@ CHANNEL_ID = 802142210581594123
 
 # 任意のチャンネルで挨拶する非同期関数を定義
 async def greet():
-    channel = client.get_channel(CHANNEL_ID)
+    channel = bot.get_channel(CHANNEL_ID)
     await channel.send('turn on')
 
 # bot起動時に実行されるイベントハンドラを定義
 @bot.event
 async def on_ready():
     await greet() # 挨拶する非同期関数を実行
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 bot.run(token)
