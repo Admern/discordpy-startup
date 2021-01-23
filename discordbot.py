b2ecd7df85bf02f9480b478e8d54e2d559c9a012
Admern <43789985+Admern@users.noreply.github.com>
2021-01-23 11:21:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,10 +10,10 @@ CHANNEL_ID = 802142210581594123
 # 任意のチャンネルで挨拶する非同期関数を定義
 async def greet():
     channel = client.get_channel(CHANNEL_ID)
-    await channel.send('おはよう！')
+    await channel.send('turn on')
 
 # bot起動時に実行されるイベントハンドラを定義
-@client.event
+@bot.event
 async def on_ready():
     await greet() # 挨拶する非同期関数を実行
 
