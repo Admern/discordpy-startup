3724032dc11ea1a8282a37301dc5c68608b74501
Admern <43789985+Admern@users.noreply.github.com>
2021-01-29 13:20:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -56,11 +56,6 @@ async def on_message(message):
     channel = client.get_channel(CHANNEL_ID)
     await channel.send(f'現在時刻'{now}'です')  
 
-    if message.content == "test":
-        await message.channel.send(f'{message.channel})
-        return
-    await message.channel.send("received a message")
-
 #        if message.content == "test":
 #            await message.channel.send(f'{message.channel})
 #            return
